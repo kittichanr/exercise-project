2867bec19dad97656ccb3fca993be5e66885ef92
kittichanr <petchkubbb@gmail.com>
2024-04-20 17:04:09 +0700

    encrypt project

---
+++ encrypt-decrypt-project/src/app.controller.ts
@@ -0,0 +1,24 @@
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { AppService } from './app.service';
+import { DataDto, PayloadDto, ResponseDecryptDto, ResponseEncryptDto } from './dto';
+
+@Controller('get-encrypt-data')
+export class EncryptController {
+  constructor(private readonly appService: AppService) { }
+
+  @Post()
+  @HttpCode(200)
+  async getEncrypt(@Body() payload: PayloadDto): Promise<ResponseEncryptDto> {
+    return await this.appService.getEncrypt(payload);
+  }
+}
+
+@Controller('get-decrypt-data')
+export class DecryptController {
+  constructor(private readonly appService: AppService) { }
+
+  @Post()
+  async getDecrypt(@Body() data: DataDto): Promise<ResponseDecryptDto> {
+    return await this.appService.getDecrypt(data);
+  }
+}
